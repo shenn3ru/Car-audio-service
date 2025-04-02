b14e2d8c4c9f5f20743533c6f5e260adc95b86cc
Catalina Psv <cocieru.catalina07@gmail.com>
2025-04-02 09:24:39 +0300

    Fixed style on images

--- java.js
+++ java.js
@@ -180,32 +180,3 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-const style = document.createElement('style');
-style.innerHTML = `
-.image-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.8);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    z-index: 1000;
-}
-
-.enlarged-image {
-    max-width: 80%;
-    max-height: 70%;
-    border: 5px solid white;
-    border-radius: 10px;
-}
-
-.image-comment {
-    color: white;
-    font-size: 18px;
-    margin-top: 10px;
-}`;
-document.head.appendChild(style);
